5ddece78dd6f811f73971470762c49748e7f1609
yyround <55855392+yyround@users.noreply.github.com>
2023-09-20 08:46:36 +1000

    Update Webcomponent.js

--- SAC/Webcomponent.js
+++ SAC/Webcomponent.js
@@ -1,7 +1,7 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<button type="button" id="myBtn">Helper Button</button>` ;   
+    `<button type="button" id="myBtn">Helper Button GY</button>` ;   
    
     class PerformanceHelp extends HTMLElement {
         constructor() {
